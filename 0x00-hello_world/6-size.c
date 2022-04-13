3caa1f96fcd98cf31e632297dd6810062f4eed68
gaszoelina <gmendy@gkonnectfi.com>
2022-04-13 04:46:53 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char; %lu byte(s)\n", sizeof(acharacter));
+
+	printf("Size of a int; %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a lon int; %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int; %lu byte(s)\n", sizeof(alonlong));
+	
+	printf("Size of a float; %lu byte(s)\n", sizeof(float));
+	return  (0);
+
+}
