f0fddd7f6dd69c417a612a27b56a43fb6dd65b98
gaszoelina <gmendy@gkonnectfi.com>
2022-04-13 05:15:25 -0700

    6-size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long alonglong;
 	float afloat;
 
-	printf("Size of a char; %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int; %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a lon int; %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int; %lu byte(s)\n", sizeof(alonlong));
-	printf("Size of a float; %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a char; %p byte(s)\n", sizeof(acharacter));
+	printf("Size of an int; %p byte(s)\n", sizeof(ainteger));
+	printf("Size of a lon int; %p byte(s)\n", sizeof(along));
+	printf("Size of a long long int; %p byte(s)\n", sizeof(alonlong));
+	printf("Size of a float; %p byte(s)\n", sizeof(afloat));
 	return  (0);
 
 }
