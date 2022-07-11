ed88ca18c9d22eb691823c741b865876c5ab351f
gaszoelina <gasza.mendy@gmail.com>
2022-07-11 06:48:14 -0700

    updated

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void){
+	char ch;
+	for (ch='A';ch<='Z'; ch++){
+		printf("%c ",ch);
+	}
+
+	return 0;
+
+}
+
