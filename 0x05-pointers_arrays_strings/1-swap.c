61b78ec6c08e0b9c111885396d75a06c3f0039f6
gaszoelina <gasza.mendy@gmail.com>
2022-07-15 10:08:53 -0700

    swap parameters

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int -> given two integers swap the value they are holding
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
