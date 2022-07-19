d7d3a9882641ff95a7d4daaf10a30b0307806eac
gaszoelina <gasza.mendy@gmail.com>
2022-07-19 09:52:48 -0700

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper -> string to uppercase
+ * @s: string
+ * Return: string in capital
+ */
+
+char *string_toupper(char *s)
+{
+
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
+}
